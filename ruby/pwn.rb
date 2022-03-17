1439cb24054d8df9b55f63b788972a6ef90702d3
shurizzle <me@shurizzle.dev>
2022-03-18 00:09:12 +0100

    Shrinking ruby solution

--- ruby/pwn.rb
+++ ruby/pwn.rb
@@ -1,3 +1,3 @@
 require 'socket'
 
-TCPSocket.open('piecewise.challs.cyberchallenge.it',9110){|s|s.each{|line,_=puts(line)|s.write(line =~ /^Please send me the number (\d+) as a (64|32)-bit (big|little)-endian /?[$1.to_i].pack(({32=>{big:'L>',little:'L<'},64=>{big:'Q>',little:'Q<'}})[$2.to_i][$3.to_sym]):"\n")&&puts(s.gets)}}
+TCPSocket.open('piecewise.challs.cyberchallenge.it',9110){|s|s.each{|line,_=puts(line)|s.write(line=~/^Please send me the number (\d+) as a (64|32)-bit (big|little)-endian /?[$1.to_i].pack(({32=>'L',64=>'Q'}[$2.to_i])+({big:'>',little:'<'}[$3.to_sym])):"\n")&&puts(s.gets)}}
